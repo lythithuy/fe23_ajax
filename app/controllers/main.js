5a37de58f808ee7bbd99ddf531bfba34cf0db626
lythithuy <lythithuthuy2008@gmail.com>
2019-06-02 16:28:06 +0700

    them chuc nang tim  kiem

--- app/controllers/main.js
+++ app/controllers/main.js
@@ -17,7 +17,7 @@ $(document).ready(function() {
 
   function test(){}
   function taoBranch(){}
-
+  function timKiem(){}
   function taoBang(mangNguoiDung) {
     var tblBody = $("#tblDanhSachNguoiDung");
     var content = "";
